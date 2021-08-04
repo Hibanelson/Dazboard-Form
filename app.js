9de1149bfbb4a215f945671bbe33d3f2d11635f4
Hibanelson <76537005+Hibanelson@users.noreply.github.com>
2021-08-04 15:49:03 +0200

    Update app.js

--- app.js
+++ app.js
@@ -31,9 +31,46 @@ function addFunctionThree() {
     q++;
     document.getElementById("quantity3").innerHTML = q;
 }
-function displayNav() {
-    document.getElementById("nav").style.display = "flex";
-}
-function hideNav() {
-    document.getElementById("nav").style.display = "none";
-}
+function openNav() {
+    document.getElementById("mySidebar").style.width = "100%";
+    document.getElementById("mySidebar").style.height = "100%";
+    document.getElementById("main").style.position = "relative";
+    document.getElementById("main").style.zIndex = "1";
+    document.getElementById("main").style.marginLeft = "70px";
+    document.getElementById("main").querySelectorAll("div")[0].style.minWidth = "315px";
+    document.getElementById("main").querySelectorAll("div")[0].style.overflowX = "hidden";
+    document.getElementById("cont").style.width = "70px";
+    document.getElementById("cont").style.height = "100%";
+    document.getElementById("cont").style.display = "flex";
+    document.getElementById("cont").style.justifyContent = "space-between";
+    document.getElementById("cont").style.flexDirection = "column";
+    document.getElementById("cont").style.backgroundColor = "white";
+    document.getElementById("cont").style.borderBottom = "1px solid #E1E1E1";
+    document.getElementById("mySidebar").querySelectorAll("ul")[0].style.display = "flex";
+    document.getElementById("mySidebar").querySelectorAll("ul")[0].style.flexWrap = "wrap";
+    document.getElementById("mySidebar").querySelectorAll("ul")[0].style.justifyContent = "center";
+    document.getElementById("mySidebar").querySelectorAll("ul")[0].style.alignItems = "center";
+    document.getElementById("mySidebar").querySelectorAll("ul")[0].style.width = "100%";
+    document.getElementById("mySidebar").querySelectorAll("ul")[0].style.marginTop = "40px";
+    document.getElementById("mySidebar").querySelectorAll("ul")[0].querySelectorAll("li")[0].style.width = "35px";
+    document.getElementById("mySidebar").querySelectorAll("ul")[0].querySelectorAll("li")[0].style.height = "35px";
+    document.getElementById("mySidebar").querySelectorAll("ul")[0].querySelectorAll("li")[0].style.backgroundColor = "red";
+    document.getElementById("mySidebar").querySelectorAll("ul")[0].querySelectorAll("li")[0].style.textAlign = "center";
+    document.getElementById("mySidebar").querySelectorAll("ul")[0].querySelectorAll("li")[0].style.paddingTop = "5px";
+    document.getElementById("mySidebar").querySelectorAll("ul")[0].querySelectorAll("li")[0].style.marginBottom = "40px";
+    document.getElementById("mySidebar").querySelectorAll("ul")[0].querySelectorAll("li")[0].querySelectorAll("a")[0].style.color = "white";
+    document.getElementById("mySidebar").querySelectorAll("ul")[0].querySelectorAll("li")[0].querySelectorAll("a")[0].style.fontWeight = "bold";
+    document.getElementById("mySidebar").querySelectorAll("ul")[0].querySelectorAll("li")[0].querySelectorAll("a")[0].style.fontSize = "20px";
+    document.getElementById("mySidebar").querySelectorAll("ul")[1].style.display = "flex";
+    document.getElementById("mySidebar").querySelectorAll("ul")[1].style.flexWrap = "wrap";
+    document.getElementById("mySidebar").querySelectorAll("ul")[1].style.justifyContent = "center";
+    document.getElementById("mySidebar").querySelectorAll("ul")[1].style.alignItems = "center";
+    document.getElementById("mySidebar").querySelectorAll("ul")[1].style.width = "100%";
+    document.getElementById("mySidebar").querySelectorAll("ul")[1].style.marginTop = "40px";
+    document.getElementById("mySidebar").querySelectorAll("ul")[1].style.backgroundColor = "white";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
